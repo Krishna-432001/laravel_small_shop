d1b595f6915d215a994e000ec31e0ee966fa80bf
Krishna-432001 <krishnapriya432001@gmail.com>
2024-08-08 13:46:24 +0530

    Update CityController.php

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -9,6 +9,33 @@ use App\Models\City;
 
 class CityController extends Controller
 {
+    public function index()
+    {
+        $cities = City::all();
+        return response()->json($cities);
+    }
+
+    public function cityInfoPage()
+    {
+        return view('city-deatails');
+    }
+
+    public function getCity($cityName)
+    {
+        $city = City::where('name' , $cityName)->with('state.country')->first();
+
+        if($city) {
+            $state = $city->state;
+            $country = $state->country;
+
+            return response()->json([
+                'city'  => $city,
+                'state' => $state,
+                'country' => $country,
+            ]);
+        }
+    }
+
     public function showCityDetails($cityName)
     {
         $city = City::where('name', $cityName)->with('state.country')->first();
