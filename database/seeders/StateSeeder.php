52d13f8ed2871145bd171a1913dd14c93f5fbb5e
Krishna-432001 <krishnapriya432001@gmail.com>
2024-08-08 12:15:34 +0530

    Create StateSeeder.php

---
+++ database/seeders/StateSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Country;
+use App\Models\State;
+
+class StateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $country = Country::where('name','india')->first();
+        $states = [
+        [$country->id, "Andaman and Nicobar Islands",  "AN"],
+        [$country->id, "Andhra Pradesh",  "AP"],
+        [$country->id, "Arunachal Pradesh",  "AR"],
+        [$country->id, "Assam",  "AS"],
+        [$country->id, "Bihar",  "BR"],
+        [$country->id, "Chandigarh",  "CH"],
+        [$country->id, "Chhattisgarh",  "CT"],
+        [$country->id, "Dadra and Nagar Haveli and Daman and Diu",  "DN"],
+        [$country->id, "Delhi",  "DL"],
+        [$country->id, "Goa",  "GA"],
+        [$country->id, "Gujarat",  "GJ"],
+        [$country->id, "Haryana",  "HR"],
+        [$country->id, "Himachal Pradesh",  "HP"],
+        [$country->id, "Jharkhand",  "JH"],
+        [$country->id, "Karnataka",  "KA"],
+        [$country->id, "Kerala",  "KL"],
+        [$country->id, "Ladakh",  "LA"],
+        [$country->id, "Lakshadweep",  "LD"],
+        [$country->id, "Madhya Pradesh",  "MP"],
+        [$country->id, "Maharashtra",  "MH"],
+        [$country->id, "Manipur",  "MN"],
+        [$country->id, "Meghalaya",  "ML"],
+        [$country->id, "Mizoram",  "MZ"],
+        [$country->id, "Nagaland",  "NL"],
+        [$country->id, "name" => "Odisha",  "OR"],
+        [$country->id, "Puducherry",  "PY"],
+        [$country->id, "Punjab",  "PB"],
+        [$country->id, "Rajasthan",  "RJ"],
+        [$country->id, "Sikkim",  "SK"],
+        [$country->id, "Tamil Nadu",  "TN"],
+        [$country->id, "Telangana",  "TG"],
+        [$country->id, "Tripura","TR"],
+        [$country->id, "Uttar Pradesh", "UP"],
+        [$country->id, "Uttarakhand","UT"],
+        [$country->id, "West Bengal",  "WB"],
+        [$country->id, "Jammu and Kashmir", "JK"],
+       ];
+       foreach ($states as $stateData)
+       {
+        State::insert([
+            'country_id' =>$stateData[0],
+            'name' => $stateData[1],
+            'code' =>$stateData[2],
+        ]);
+       }
+    }
+}
