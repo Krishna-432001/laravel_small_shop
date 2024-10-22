b6a171ca570476ccddc387176a9e45335084d9b7
krishna-432001 <krishnapriya432001@gmail.com>
2024-10-22 13:07:05 +0530

    ui ux logic added footer

--- resources/views/frontend/layout/footer.blade.php
+++ resources/views/frontend/layout/footer.blade.php
@@ -1,10 +1,15 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
 <footer class="bg-dark text-white py-4">
     <div class="container">
         <div class="row">
+            <!-- About Us Section -->
             <div class="col-md-4">
                 <h5>About Us</h5>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla sit amet volutpat.</p>
             </div>
+
+            <!-- Links Section -->
             <div class="col-md-4">
                 <h5>Links</h5>
                 <ul class="list-unstyled">
@@ -13,13 +18,42 @@
                     <li><a href="#" class="text-white">Pricing</a></li>
                 </ul>
             </div>
+
+            <!-- Contact Section -->
             <div class="col-md-4">
                 <h5>Contact</h5>
                 <p>Email: example@example.com</p>
                 <p>Phone: (123) 456-7890</p>
+
+                <!-- Social Media Links -->
+                <h5>Follow Us</h5>
+                <ul class="list-unstyled d-flex">
+                    <li class="me-3">
+                        <a href="https://facebook.com" target="_blank" class="text-white">
+                            <i class="fab fa-facebook fa-2x"></i>
+                        </a>
+                    </li>
+                    <li class="me-3">
+                        <a href="https://twitter.com" target="_blank" class="text-white">
+                            <i class="fab fa-twitter fa-2x"></i>
+                        </a>
+                    </li>
+                    <li class="me-3">
+                        <a href="https://instagram.com" target="_blank" class="text-white">
+                            <i class="fab fa-instagram fa-2x"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://linkedin.com" target="_blank" class="text-white">
+                            <i class="fab fa-linkedin fa-2x"></i>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
+
+    <!-- Footer Bottom Section -->
     <div class="text-center py-3">
         &copy; 2024 Your Company
     </div>
